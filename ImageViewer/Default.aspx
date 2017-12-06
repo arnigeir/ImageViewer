8475dd26a5e29be8ec905e7e71f3e89a98e4aace
Arni Geir Sigurdsson <arni.geir.sigurdsson@gmail.com>
2017-12-06 21:49:50 +0000

    initial commit

---
+++ ImageViewer/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ImageViewer.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Myndaskoðari Vegagerðarinnar</title>
+    <style type="text/css">
+        .Content{
+            padding:1em;
+        }
+        .Title{
+            font-size:xx-large;
+            padding-bottom:1em;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="Content">
+        <div class="Title">
+           <asp:Label ID="Title_Label" runat="server" ></asp:Label>
+        </div>
+        <div class="Image">
+            <img id="chart_img" src="images/chart.png" runat="server"/>
+         </div>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ ImageViewer/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ImageViewer
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String imagepath = WebConfigurationManager.AppSettings["imagefolder"];
+            String stationid = Request["stationid"] as String;
+            String sensorid = Request["sensorid"] as String;
+            String filename = imagepath + "missing.png";
+            String title = "Númer mælistöðvar eða skynjara vantar";
+
+            if (!String.IsNullOrEmpty(stationid) && !String.IsNullOrEmpty(sensorid))
+            {
+                filename = imagepath + "Sensor_" + stationid + "_" + sensorid + ".png";
+                title = "Spá byggð á skynjara númer " + sensorid + " fyrir mælistöð númer " + stationid ;
+            }
+            chart_img.Src = filename;
+            Title_Label.Text = title;
+
+
+        }
+    }
+}
